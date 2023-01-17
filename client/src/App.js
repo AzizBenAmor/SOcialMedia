78230a0c5547e911edab6798a829305a7a9f200f
AzizBenAmor <azizbenamor1288@gmail.com>
2023-01-17 10:18:21 +0100

    beta

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter, Navigate, Routes , Route } from 'react-router-dom';
+import homePage from 'scenes/homePage';
+import navBar from 'scenes/navBar';
+import loginPage from 'scenes/loginPage';
+import profilePage from 'scenes/profilePage';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+     
+    </div>
+  );
+}
+
+export default App;
